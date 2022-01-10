b6c8fc290cd7c38814f5cc11cf7624762c9fb022
Taylor Noj <taylornoj@gmail.com>
2022-01-10 19:52:25 +0000

    test that it not valid without name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Product, type: :model do
       expect(subject).to be_valid
     end
 
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+      print ".errors.full_messages === "
+      print subject.errors.full_messages
+    end
+
 
   end
 end
