36109371407c383d14d4cc1dc96f3cc8a2bdb5ab
Taylor Noj <taylornoj@gmail.com>
2022-01-10 18:14:05 +0000

    all attributes valid test passes however will rework for error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    let (:category) { Category.create(name: 'testCategory') }
+    subject { Product.new(name: 'test', price_cents: 9000, quantity: 100, category_id: category.id) }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+
+  end
 end
