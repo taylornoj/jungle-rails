cb1f34d68424a90ec4ee5d673fd027b822230e46
Taylor Noj <taylornoj@gmail.com>
2022-01-10 23:27:44 +0000

    update user.rb model with validates, begin testing in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    subject do
+      User.new(
+        id: 1,
+        name: 'first',
+        email: 'test@test.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+    
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+      print ".errors.full_messages === "
+      print subject.errors.full_messages
+    end
+
+   
+
+  end
+ 
+
 end
+
+
