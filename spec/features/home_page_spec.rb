93804e52564db63d5953ca951284655c812f047d
Taylor Noj <taylornoj@gmail.com>
2022-01-11 20:21:45 +0000

    add more robust testing

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -15,15 +15,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       )
     end
   end
-  
+
   scenario "They see all products" do
     # ACT
     visit root_path
 
-    # DEBUG / VERIFY
-    save_screenshot
+    # commented out b/c it's for debugging only
+    # save_and_open_screenshot
 
-    expect(page).to have_css 'article.product'
+    expect(page).to have_css 'article.product', count: 10
   end
 
 end
